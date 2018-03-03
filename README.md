71a04493b5b5b8ea8e5b5ef9a907fe1926dc0646
madosuki <yurikasamaaikatsu@gmail.com>
2018-03-03 22:41:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tail Call Recursion Factorial
+勉強がてら末尾再帰で階乗を計算するスクリプトを書きました。  
+通常の再帰関数も書いてあるので、違いを試せます。
+```
+sbcl --script fact.lisp
+```
---
+++ fact.lisp
@@ -0,0 +1,19 @@
+(defun tailcall_fact(n &optional (result 1))
+  (cond ((= n 1)
+         result)
+        ((/= n 1)
+         (tailcall_fact (- n 1) (* n result)))))
+
+(defun recursion_fact(n)
+  (cond ((= n 1)
+         1)
+        ((/= n 1)
+         (* n (recursion_fact (- n 1))))))
+
+(defun main()
+  (format t "~D~%" (tailcall_fact 100000))
+  ;; below normal recursion expression is stack error.
+  ;; (format t "~D~%" (recursion_fact 100000))
+  )
+
+(main)
