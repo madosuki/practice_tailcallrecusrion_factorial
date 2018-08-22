4832a5466716302939f4a9556c1014e72a3d69f4
madosuki <yurikasamaaikatsu@gmail.com>
2018-08-22 09:36:26 +0900

    fix layout at ltk

--- fact_gui.lisp
+++ fact_gui.lisp
@@ -1,6 +1,10 @@
 (ql:quickload 'ltk)
+
 (defpackage :cl-ltk
-  (:use :common-lisp :ltk))
+  (:use
+   :common-lisp
+   :ltk))
+
 (in-package :cl-ltk)
 
 (defun tailcall_fact(n &optional (result 1))
@@ -12,28 +16,33 @@
 (defvar *x* 10)
 (defvar *y* 0)
 (defvar *min* 640)
+(defvar *width* 800)
+(defvar *height* 600)
+(defvar *title* "Factorial Calculator with GUI")
 
-(defun main()
+(defun main()  
   (with-ltk()
+    (wm-title *tk* *title*)
     
-    (wm-title *tk* "Factorial Calculator with GUI")
-    
-    (let* ((f (make-instance 'frame))
-           (lb (make-instance 'label :master f :text "Please input number"))
-           (entry (make-instance 'entry :master f))
-           (button (make-instance 'button :master f :text "Start Calculation!"))
-           (output (make-instance 'label :master f :wraplength *min*)))
+    (let* ((sc (make-instance 'scrolled-canvas))
+           (c (canvas sc))
+           (lb (make-instance 'label :master c :text "Please input number"))
+           (entry (make-instance 'entry :master c))
+           (button (make-instance 'button :master c :text "Start Calculation!"))
+           (output (make-instance 'label :master c :wraplength *min*)))
       
-      (pack f)
+      (pack sc :expand 1 :fill :both)
       (pack lb)
       (pack entry)
       (pack button)
       (pack output)
       
-      (configure f :borderwidth 3)
-      (configure f :relief :sunken)
+      ;; (configure f :borderwidth 3)
+      ;; (configure f :relief :sunken)
+
+      (scrollregion c 0 0 *width* *height*)
       
-      (set-geometry *tk* 800 600 0 0)
+      (set-geometry *tk* *width* *height* 0 0)
       
       (bind button "<ButtonRelease-1>"
             (lambda(e)
@@ -42,5 +51,4 @@
                 (setf (ltk:text output)
                       (tailcall_fact
                        (parse-integer (ltk:text entry))))))))))
-
 (main)
